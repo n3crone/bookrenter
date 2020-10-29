9b1ccd777c8362c0075ff1ee24ef3ccddff329f5
Patryk Kozłowski <patryk.kozlowski@sts.pl>
2020-10-29 22:26:20 +0100

    feature: vue component

---
+++ assets/js/home.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from '@/pages/home';
+
+new Vue({
+    render: (h) => h(App),
+}).$mount('#home');
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('home', './assets/js/home.js')
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
